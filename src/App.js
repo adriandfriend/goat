76da4de871f4e7b817a82fa7f5ba3b593273ed57
Adrian Friend <adrianf@khws.co.uk>
2019-03-01 21:20:53 +0000

    Add accuracy options

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import './App.css'
-// import useGeolocation from './common/useGeoLocation'
 import useNetworkStatus from './common/useNetworkStatus'
 import Location from './components/location'
 import {
@@ -11,6 +10,7 @@ import {
 const App = () => {
 
   const [coords, setCoords] = useState()
+  const [highAccuracy, setHighAccuracy] = useState(false)
   const network = useNetworkStatus()
 
   const onEvent = (event) => {
@@ -22,8 +22,22 @@ const App = () => {
     })
   }
 
+  const onEventError = (event) => {
+    console.log(event)
+  }
+
+  const options = {
+    enableHighAccuracy: {highAccuracy},
+    timeout: 5000,
+    maximumAge: 0
+  }
+
   const onClick = async () => {
-    await navigator.geolocation.getCurrentPosition(onEvent)
+    await navigator.geolocation.getCurrentPosition(onEvent, onEventError, options)
+  }
+
+  const handleCheckedChange = (event) => {
+    setHighAccuracy(event.target.checked)
   }
 
   return (
@@ -32,6 +46,10 @@ const App = () => {
         <h1>G.O.A.T <span role="img" aria-label="goat emoji">🐐</span></h1>
         <h4>(Geolocation Output Accuracy Tester)</h4>
         <p>Effective Network Type {network.effectiveType}</p>
+        <label htmlFor="accuracy">
+          <input type="checkbox" id="accuracy" onChange={handleCheckedChange} />
+          Enable High Accuracy
+        </label>
         <button
           className="btn"
           onClick={onClick}
